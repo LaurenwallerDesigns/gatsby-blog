301deda5a5eedec625af605322dc59867e5807ba
lauren <waller.lauren.lw@gmail.com>
2020-12-06 19:45:58 -0500

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby'
+import Layout from '../components/layout';
+
+export default () => (
+    <Layout>
+        <h1> Hello World I am Lauren</h1>
+        <Link to="/"> &larr; Back to Home</Link>
+    </Layout>
+)
